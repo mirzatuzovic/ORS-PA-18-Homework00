b0d86d108393e780ba8cf810940dc10dfd97f6c8
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-02-11 15:03:23 +0100

    int i str

---
+++ hello 1.py
@@ -0,0 +1,12 @@
+korisnik_unio_prvi = input("Unesite prvi broj: ")
+korisnik_unio_drugi = input("Unesite drugi broj: ")
+
+if (not korisnik_unio_prvi.isnumeric() or not korisnik_unio_drugi.isnumeric()):
+    print("Nijeste unijeli dobar broj")
+    quit()
+
+broj_prvi = int(korisnik_unio_prvi)    #prebacanje stringa u integer
+broj_drugi = int(korisnik_unio_drugi)
+
+zbir = broj_prvi + broj_drugi
+print(zbir)
